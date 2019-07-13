a0f42f9f56b0adf98b94ebfe2b5af9e44f5ec943
quyet <quyet@whl.travel>
2019-07-13 09:41:22 +0700

    css mobile

--- page-home.php
+++ page-home.php
@@ -23,7 +23,6 @@
     #social {
         background: url('<?php echo get_template_directory_uri(); ?>/img/bg-social.jpg') center center no-repeat;
         background-size: cover;
-        height: 334px;
         color: white;
     }
     #social .col-lg-4 {
@@ -100,16 +99,17 @@
     $videoUrl = get_template_directory_uri().'/video/home.mp4';
 ?>
 
-<header id="video" class="hidden-md-down">
-    <div class="overlay"></div>
-    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
-        <source src="<?php echo $videoUrl; ?>" type="video/mp4">
-    </video>
-    <div class="row">
-        <div class="col-md-12 video-top">
-            
-        </div>
-        <div class="col-md-12 video-content" style="background: url('<?php echo get_template_directory_uri(); ?>/img/optin-bar-bg.png")  center center no-repeat;'>
+<section>
+    <header id="video" class="hidden-md-down">
+        <div class="overlay"></div>
+        <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+            <source src="<?php echo $videoUrl; ?>" type="video/mp4">
+        </video>
+        <div class="row">
+            <div class="col-md-12 video-top">
+
+            </div>
+            <div class="col-md-12 video-content" style="background: url('<?php echo get_template_directory_uri(); ?>/img/optin-bar-bg.png")  center center no-repeat;'>
             <div class="container">
                 <div class="row">
                     <div class="video-content-image">
@@ -157,15 +157,17 @@
                 </div>
             </div>
         </div>
-    </div>
-</header>
-<header id="image">
-    <div class="row">
-        <div class="col-lg-12">
-            <!--<div class="video-bg" style="background: url('<?php /*echo get_template_directory_uri(); */?>/img/feature-one-bg.jpg")  center center no-repeat;'></div>-->
-            <img src="<?php echo get_template_directory_uri(); ?>/img/video-bg.jpg"); ?>">
-    </div>
-</header>
+        </div>
+    </header>
+    <header id="image">
+        <div class="row">
+            <div class="col-lg-12">
+                <!--<div class="video-bg" style="background: url('<?php /*echo get_template_directory_uri(); */?>/img/feature-one-bg.jpg")  center center no-repeat;'></div>-->
+                <img src="<?php echo get_template_directory_uri(); ?>/img/video-bg.jpg"); ?>">
+            </div>
+    </header>
+</section>
+
 
 
 <!--Meet Peng Joon-->
@@ -434,44 +436,46 @@
 </section>
 
 <section id="form-footer">
-    <div class="container">
-        <div class="row">
-            <div class="video-content-image">
-                <img src="/wp-content/uploads/2019/05/hinhdaidien-copy-e1559142553844.png" />
-            </div>
+    <div class="row">
+        <div class="container">
             <div class="row">
-                <div class="col-md-12 video-content-block">
-                    <div class="video-content-text">
-                        <h3>ĐỪNG MỞ QUÁN NẾU CHƯA BIẾT QUY TRÌNH SETUP</h3>
-                        <h5>Nhận quy trình SETUP COFFEE từ A-Z</h5>
-                    </div>
+                <div class="video-content-image">
+                    <img src="/wp-content/uploads/2019/05/hinhdaidien-copy-e1559142553844.png" />
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="video-content-text">
+                            <h3>ĐỪNG MỞ QUÁN NẾU CHƯA BIẾT QUY TRÌNH SETUP</h3>
+                            <h5>Nhận quy trình SETUP COFFEE từ A-Z</h5>
+                        </div>
 
-                    <div class="row">
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Họ tên" id="full_name">
+                        <div class="row">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Họ tên" id="full_name">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Số điện thoại"  id="phone">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Số điện thoại"  id="phone">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Email" id="email">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Email" id="email">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <button type="button" class="btn btn-danger">Nhận ngay</button>
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <button type="button" class="btn btn-danger">Nhận ngay</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
