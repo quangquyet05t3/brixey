1b095d22a1e2f07cf584e974ceff3cbcb889f74d
quyet <quyet@whl.travel>
2019-07-14 05:08:46 +0700

    .

--- page-home-2.php
+++ page-home-2.php
@@ -37,70 +37,68 @@
         width: 30px;
     }
 
-    #footer {
-        background: url('<?php echo get_template_directory_uri(); ?>/img/bg-footer.png') center center no-repeat;
-        background-size: cover;
-    }
-
 </style>
 
 <!--Section Nav-->
-<nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse" id="nav-main">
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <a class="navbar-brand" href="#">
-        <img src="/wp-content/uploads/2019/05/peng-joon-logo-copy-copy-e1559135091999.png">
-    </a>
+<section id="header">
+    <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse" id="nav-main">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a class="navbar-brand" href="#">
+            <img src="/wp-content/uploads/2019/05/peng-joon-logo-copy-copy-e1559135091999.png">
+        </a>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Dự án</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Dịch vụ</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Tin tức</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Giới thiệu</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Liên hệ</a>
-            </li>
-        </ul>
-        <ul class="navbar-nav mr-auto">
-            <li>
-                <a class="nav-link" href="/facebook">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/fb-icon-sm.png" />
-                </a>
-            </li>
-            <li>
-                <a class="nav-link" href="/instagram">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/instagram-icon-sm.png" />
-                </a>
-            </li>
-            <li>
-                <a class="nav-link" href="/youtube">
-                    <img src="<?php echo get_template_directory_uri(); ?>/img/youtube-icon-sm.png" />
-                </a>
-            </li>
-        </ul>
-    </div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dự án</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Dịch vụ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Tin tức</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Giới thiệu</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Liên hệ</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav mr-auto">
+                <li>
+                    <a class="nav-link" href="/facebook">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/fb-icon-sm.png" />
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="/instagram">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/instagram-icon-sm.png" />
+                    </a>
+                </li>
+                <li>
+                    <a class="nav-link" href="/youtube">
+                        <img src="<?php echo get_template_directory_uri(); ?>/img/youtube-icon-sm.png" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</section>
 
-<!-- The video lam sau -->
+<!-- The video -->
 <?php
     $videoUrl = get_template_directory_uri().'/video/home.mp4';
 ?>
+
 <section>
     <header id="video" class="hidden-md-down">
         <div class="overlay"></div>
@@ -210,6 +208,7 @@
         </div>
     </div>
 </section>
+
 
 <!----------Article--------->
 <section>
@@ -437,44 +436,46 @@
 </section>
 
 <section id="form-footer">
-    <div class="container">
-        <div class="row">
-            <div class="video-content-image">
-                <img src="/wp-content/uploads/2019/05/hinhdaidien-copy-e1559142553844.png" />
-            </div>
+    <div class="row">
+        <div class="container">
             <div class="row">
-                <div class="col-md-12">
-                    <div class="video-content-text">
-                        <h3>ĐỪNG MỞ QUÁN NẾU CHƯA BIẾT QUY TRÌNH SETUP</h3>
-                        <h5>Nhận quy trình SETUP COFFEE từ A-Z</h5>
-                    </div>
+                <div class="video-content-image">
+                    <img src="/wp-content/uploads/2019/05/hinhdaidien-copy-e1559142553844.png" />
+                </div>
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="video-content-text">
+                            <h3>ĐỪNG MỞ QUÁN NẾU CHƯA BIẾT QUY TRÌNH SETUP</h3>
+                            <h5>Nhận quy trình SETUP COFFEE từ A-Z</h5>
+                        </div>
 
-                    <div class="row">
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Họ tên" id="full_name">
+                        <div class="row">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Họ tên" id="full_name">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Số điện thoại"  id="phone">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Số điện thoại"  id="phone">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <input class="form-control" type="text" placeholder="Email" id="email">
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <input class="form-control" type="text" placeholder="Email" id="email">
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="col-md-3 p-1">
-                            <div class="form-group row">
-                                <div class="col-12">
-                                    <button type="button" class="btn btn-danger">Nhận ngay</button>
+                            <div class="col-md-3 p-1">
+                                <div class="form-group row">
+                                    <div class="col-12">
+                                        <button type="button" class="btn btn-danger">Nhận ngay</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -486,7 +487,11 @@
 </section>
 
 <section id="footer">
-    <div class="container">
+
+    <div class="row">
+        <div class="col-170">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/bg-footer-left.png">
+        </div>
         <div class="col">
             <ul class="nav justify-content-center">
                 <li class="nav-item">
@@ -515,8 +520,13 @@
                 Peng Joon © 2018 - Terms of Use & Privacy Policy
             </div>
         </div>
+        <div class="col-170">
+            <img src="<?php echo get_template_directory_uri(); ?>/img/bg-footer-right.png">
+        </div>
     </div>
 </section>
+
+
 
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="<?php echo get_template_directory_uri() ?>/vendor/jquery/dist/jquery.min.js"></script>
