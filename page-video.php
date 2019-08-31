bbcf9f7c32799321b4bdeb11d783ef2cbf5c9f8a
quyet <quyet@whl.travel>
2019-08-31 07:39:34 +0700

    video

--- page-video.php
+++ page-video.php
@@ -14,22 +14,67 @@ $args = array(
     'posts_per_page' => $post_per_page
 );
 $lastBlog = new WP_Query($args);
+$current_year = date('Y', time());
+$previous_year = (string) (((int)$current_year)-1);
 if( $lastBlog->have_posts() ):
     while( $lastBlog->have_posts() ): $lastBlog->the_post(); ?>
         <?php
-            $current_year = date('Y', time());
+
             list(, , $year) = explode('/', get_the_date());
             $list_video[$year][] = array(
+                'year' => $year,
                 'title' => get_the_title(),
-                'link_youtube' => get_field('link_youtube')
+                'embed_youtube' => get_field('embed_youtube')
             );
         ?>
     <?php endwhile;
 endif;
 wp_reset_postdata();
+
+
+
+/*
+$list_video = array();
+$current_year = date('Y', time());
+
+for($i=1;$i<=3;$i++) {
+    $list_video['2016'][] = array(
+        'year' => '2016',
+        'title' => 'Video 2016 '.$i,
+        'embed_youtube' => '<iframe width="560" height="250" src="https://www.youtube.com/embed/Gr1pREAm6Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    );
+}
+
+for($i=1;$i<=3;$i++) {
+    $list_video['2017'][] = array(
+        'year' => '2017',
+        'title' => 'Video 2017 '.$i,
+        'embed_youtube' => '<iframe width="560" height="250" src="https://www.youtube.com/embed/Gr1pREAm6Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    );
+}
+
+for($i=1;$i<=5;$i++) {
+    $list_video['2018'][] = array(
+        'year' => '2018',
+        'title' => 'Video 2018 '.$i,
+        'embed_youtube' => '<iframe width="560" height="250" src="https://www.youtube.com/embed/Gr1pREAm6Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    );
+}
+
+for($i=1;$i<=2;$i++) {
+    $list_video['2019'][] = array(
+        'year' => '2019',
+        'title' => 'Video 2019 '.$i,
+        'link_youtube' => 'https://www.youtube.com/embed/Gr1pREAm6Os',
+        'embed_youtube' => '<iframe width="560" height="250" src="https://www.youtube.com/embed/Gr1pREAm6Os" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+    );
+}*/
+
+
+
+krsort($list_video);
+
 ?>
-
-
 
 
 <section id="testimonials-banner">
@@ -49,159 +94,95 @@ wp_reset_postdata();
 		<div class="row">
             <?php if(isset($list_video[$current_year])) : ?>
                 <?php foreach ($list_video[$current_year] as $item) : ?>
-                   <!-- --><?php /*echo $item['title']; */?>
+                    <div class="col-lg-6">
+                        <div class="media-video-box">
+                            <?php echo $item['embed_youtube']; ?>
+                            <div class="media-video-text">
+                                <h2> <?php echo $item['title']; ?></h2>
+                            </div>
+                        </div>
+                    </div>
                 <?php endforeach; ?>
             <?php endif; ?>
-
-
-			<div class="col-lg-6">
-				<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>
-				</div>				
-			</div>
-			<div class="col-lg-6">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
 		</div>
 	</div>
 </section>
 <section id="testimonials-video">
 	<div class="container">
 		<div class="row">
-			<div class="media-press_img">
-				<h2>
-					<b></b>
-						<span>2018</span>
-					<b></b>
-				</h2>
-			</div>
-			<div class="col-lg-6">
-				<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>
-				</div>				
-			</div>
-			<div class="col-lg-6">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
+            <?php if(isset($list_video[$previous_year])) : ?>
+                <div class="media-press_img">
+                    <h2>
+                        <b></b>
+                        <span><?php echo $previous_year; ?></span>
+                        <b></b>
+                    </h2>
+                </div>
+                <?php $i=1; ?>
+                <?php foreach ($list_video[$previous_year] as $item) : ?>
+                    <?php if($i<=2): ?>
+                        <div class="col-lg-6">
+                            <div class="media-video-box">
+                                <?php echo $item['embed_youtube']; ?>
+                                <div class="media-video-text">
+                                    <h2><?php echo $item['title']; ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <?php $i++; ?>
+                <?php endforeach; ?>
+            <?php endif; ?>
 		</div>
 		<div class="row">
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
+            <?php if(isset($list_video[$previous_year])) : ?>
+                <?php $i=1; ?>
+                <?php foreach ($list_video[$previous_year] as $item) : ?>
+                    <?php if($i>2): ?>
+                        <div class="col-lg-4">
+                            <div class="media-video-box">
+                                <?php echo $item['embed_youtube']; ?>
+                                <div class="media-video-text">
+                                    <h2><?php echo $item['title']; ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif; ?>
+                    <?php $i++; ?>
+
+                <?php endforeach; ?>
+            <?php endif; ?>
 		</div>
 	</div>
 </section>
-<section id="testimonials-video-1">
-	<div class="container">
-		<div class="row">
-			<div class="media-press_img">
-				<h2>
-					<b></b>
-						<span>2017</span>
-					<b></b>
-				</h2>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<section id="testimonials-video-2">
-	<div class="container">
-		<div class="row">
-			<div class="media-press_img">
-				<h2>
-					<b></b>
-						<span>2016</span>
-					<b></b>
-				</h2>
-			</div>
-			<div class="col-lg-4">
-				<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>
-				</div>				
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-			<div class="col-lg-4">
-					<div class="media-video-box">
-					<iframe height="315" src="https://www.youtube.com/embed/oMcIcEiOtmc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-					<div class="media-video-text">
-	    				<h2>ABC4 Utah</h2>
-					</div>			
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<?php brixey_pagination($lastBlog) ?>
+<?php $i=1; ?>
+<?php foreach ($list_video as $year => $items): ?>
+    <?php if($year!=(int)$current_year && $year!=(int)$previous_year): ?>
+        <section id="testimonials-video-<?php echo $i; ?>">
+            <div class="container">
+                <div class="row">
+                    <div class="media-press_img">
+                        <h2>
+                            <b></b>
+                            <span><?php echo $year; ?></span>
+                            <b></b>
+                        </h2>
+                    </div>
+                    <?php foreach ($items as $item): ?>
+                        <div class="col-lg-4">
+                            <div class="media-video-box">
+                                <?php echo $item['embed_youtube']; ?>
+                                <div class="media-video-text">
+                                    <h2><?php echo $item['title']; ?></h2>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        </section>
+        <?php $i++; ?>
+    <?php endif; ?>
+<?php endforeach; ?>
 
 <?php get_footer() ?>
