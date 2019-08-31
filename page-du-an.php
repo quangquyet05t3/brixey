4620b323bc1e336f2304b224db376ec48d618e7e
quyet <quyet@whl.travel>
2019-09-01 04:42:41 +0700

    .

--- page-du-an.php
+++ page-du-an.php
@@ -2,135 +2,30 @@
 
 <!--Meet Peng Joon-->
 <?php
-$article1 = $article2 = $article3 = '';
-
-if( have_posts() ):
-    while( have_posts() ): the_post(); ?>
-        <?php
-        $content = get_the_content();
-        $content = str_replace(
-            array('<!-- wp:paragraph -->', '<!-- /wp:paragraph -->', '<p></p>'),
-            array('', '', ''),
-            $content
-        );
-        $articles = new SimpleXMLElement($content);
-        $article1 = (isset($articles->article[0])) ? $articles->article[0] : '';
-        $article2 = (isset($articles->article[1])) ? $articles->article[1] : '';
-        $article3 = (isset($articles->article[2])) ? $articles->article[2] : '';
-        $article4 = (isset($articles->article[3])) ? $articles->article[3] : '';
-
-        ?>
-    <?php endwhile;
-endif;
-wp_reset_postdata();
-
+if(!empty(get_option( 'sticky_posts' ))) {
+    $args = [
+        'type' => 'post',
+        'posts_per_page' => 1,
+        'post__in'  => get_option( 'sticky_posts' )  // Get article with sticky
+    ];
+    $the_query = new WP_Query($args);
+    ?>
+    <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+        <section id="about">
+            <?php get_template_part('template-parts/content-sticky', get_post_format()); ?>
+        </section>
+    <?php endwhile; ?>
+    <?php
+}
 ?>
-<section id="about-market">
-    <div class="container">
-        <div class="row">
-            <div class="about-market_box">
-                <h2><?php echo $article1->h2; ?></h2>
-                <?php
-                foreach ($article1->item as $p) {
-                    echo '<p>'.$p.'</p>';
-                }
-                ?>
-            </div>
-        </div>
-    </div>
-</section>
-<section id="about-event">
-</section>
-<section id="about-successfully">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
 
-            </div>
-            <div class="col-lg-6">
-                <h3><?php echo $article2->h3; ?></h3>
-                <h5><?php echo $article2->h5; ?></h5>
-                <?php
-                    foreach ($article2->item as $p) {
-                        echo '<p>'.$p.'</p>';
-                    }
-                ?>
-            </div>
-
-        </div>
-    </div>
-</section>
-<section id="about-event_time">
-</section >
-<section id="about-programs">
-    <div class="container">
-        <div class="row">
-            <div class="about-programs_box">
-                <h2><?php echo $article3->h2; ?></h2>
-                <div class="row">
-                    <div class="about-programs_elementor">
-                        <div class="col-lg-2 col-sm-3">
-                            <img src="/wp-content/themes/brixey/img/monitor-icon.png" alt="">
-                        </div>
-                        <div class="col-lg-10 col-sm-9">
-                            <p><?php echo $article3->item[0]; ?></p>
-                        </div>
-                    </div>
-                    <div class="about-programs_elementor">
-                        <div class="col-lg-2 col-sm-3">
-                            <img src="/wp-content/themes/brixey/img/dollar-icon.png" alt="">
-                        </div>
-                        <div class="col-lg-10 col-sm-9">
-                            <p><?php echo $article3->item[1]; ?></p>
-                        </div>
-                    </div>
-                    <div class="about-programs_elementor">
-                        <div class="col-lg-2 col-sm-3">
-                            <img src="/wp-content/themes/brixey/img/globe-icon.png"  alt="">
-                        </div>
-                        <div class="col-lg-10 col-sm-9">
-                            <p><?php echo $article3->item[2]; ?></p>
-                        </div>
-                    </div>
-                    <div class="about-programs_elementor">
-                        <div class="col-lg-2 col-sm-3">
-                            <img src="/wp-content/themes/brixey/img/mic-icon.png"  alt="">
-                        </div>
-                        <div class="col-lg-10 col-sm-9">
-                            <p><?php echo $article3->item[3]; ?>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section id="about-famous">
-</section >
-<section id="about-seemore">
-    <div class="container">
-        <div class="row">
-            <?php echo $article4->item->asXML(); ?>
-        </div>
-        <div class="row justify-content-center">
-            <div class="read-more-services">
-                <div class="container">
-                    <div class="row">
-                        <div class="read-more-arrow">
-                            <div class="arrow icon">
-                            </div>
-                        </div>
-                        <div class="read-more-button">
-                            <a href="/dich-vu/">
-                                <button type="button" class="btn btn-light">Xem thêm dịch vụ</button>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section >
-<?php brixey_pagination($lastBlog) ?>
+<?php
+    if( have_posts() ):
+        while( have_posts() ): the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile;
+    endif;
+    wp_reset_postdata();
+?>
 
 <?php get_footer() ?>
--- page-lien-he.php
+++ page-lien-he.php
@@ -6,50 +6,14 @@
         </div>
     </div>
 </section>
-<section id="contact-f">
-    <div class="section-content">
-        <div class="container">
-            <div class="row justify-content-md-center">
-                <div class="col-md-9">
-                    <div class="contact-text">
-                        <?php
-                        if( have_posts() ):
-                            while( have_posts() ): the_post(); ?>
-                                <?php get_template_part('template-parts/content-single',get_post_format()); ?>
-                            <?php endwhile;
-                        endif;
-                        wp_reset_postdata();
-                        ?>
-                    </div>
-                    <div class="hire">
-                        <h2>Hire Peng Joon</h2>
-                        <h3>Information on 1:1 Consulting &amp; Speaking<br> Engagements</h3>
-                        <span>If you’re interested in consulting or speaking with Peng Joon, his fees for events are below:</span>
-                        <table>
-                          <tr>
-                            <td>LUNCH (TO PICK HIS BRAIN)</td>
-                            <td>Starts at <strong>$2,000</strong> and on location in Kuala Lumpur</td>
-                          </tr>
-                          <tr>
-                            <td>SPEAKING ENGAGEMENTS</td>
-                            <td><strong>$25,000</strong> (keynote) or 50% back of room sales (whichever is higher)</td>
-                          </tr>
-                          <tr>
-                            <td>BUILDING A FUNNEL</td>
-                            <td><strong>$50,000</strong> and 10% of gross sales of the funnel (lifetime of the funnel)</td>
-                          </tr>
-                          <tr>
-                            <td>TRAIN YOUR TEAM</td>
-                            <td><strong>$50,000</strong> for 2 days</td>
-                          </tr>
-                        </table>
-                        <span>** prices quoted are in USD.</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+<?php
+    if( have_posts() ):
+        while( have_posts() ): the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile;
+    endif;
+    wp_reset_postdata();
+?>
 <section id="contact-img">
     
 </section>
