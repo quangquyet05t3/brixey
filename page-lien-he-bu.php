976a5ab5a54e73724ad4610ecfd4332a88a1f332
quyet <quyet@whl.travel>
2019-09-01 05:04:50 +0700

    bu

---
+++ page-lien-he-bu.php
@@ -0,0 +1,52 @@
+<section id="contact-f">
+    <div class="section-content">
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-md-9">
+                    <div class="contact-text">
+                        <h3>Liên hệ</h3>
+                        <h2><strong>ĐƠN VỊ HỖ TRỢ </strong></h2>
+                        <p><strong>VAN HIEN GROUP – CHUYÊN CÁ CẢNH GIÁ SỈ</strong><br>
+                            Trại Cá Cảnh Giá Sỉ<br>
+                            ĐC: 256/16a khu phố 2, P Thạnh xuân 25, Quận 12, HCM<br>
+                            Liên hệ : 0912.888.445 -0942.888.445<br>
+                            Cửa hàng Bán Lẻ<br>
+                            ĐC: số 290, Đường Dương Thị Mười, P Tân Thới Hiệp, Q12, HCM<br>
+                            Liên hệ: 0917.888.445</p>
+                        <p><strong>KENDESIGN – CHUYÊN GIA THIẾT KẾ NHÀ HÀNG</strong><br>
+                            VP Hà Nội: tầng 7 – số 6 – ngõ 75 Trần Thái Tông – HN<br>
+                            VP HCM: Lầu 1 Số 38L – Miếu Nổi – Bình Thạnh -HCM<br>
+                            Liên hệ: 0973.27.22.66</p>
+                        <p>Sứ mệnh của Tran Xuan Van Architects là “<strong><em>Kết nối, giữ lửa Kiến trúc, mang hạnh phúc đến mọi nhà</em></strong>”</p>
+                        <p>Slogal thể hiện sứ mệnh đã chọn: “<strong><em>Vẽ niềm tin – Xây hạnh phúc</em>”</strong></p>
+                        <p>Chúng tôi khao khát hỗ trợ, hợp tác với KTS Việt Nam cùng xây dựng nền Kiến trúc Việt Nam về đúng tinh thần bản sắc dân tộc và hội nhập Quốc tế.</p>
+                    </div>
+                    <div class="hire">
+                        <h2>Hire Peng Joon</h2>
+                        <h3>Information on 1:1 Consulting &amp; Speaking<br> Engagements</h3>
+                        <span>If you’re interested in consulting or speaking with Peng Joon, his fees for events are below:</span>
+                        <table>
+                          <tr>
+                            <td>LUNCH (TO PICK HIS BRAIN)</td>
+                            <td>Starts at <strong>$2,000</strong> and on location in Kuala Lumpur</td>
+                          </tr>
+                          <tr>
+                            <td>SPEAKING ENGAGEMENTS</td>
+                            <td><strong>$25,000</strong> (keynote) or 50% back of room sales (whichever is higher)</td>
+                          </tr>
+                          <tr>
+                            <td>BUILDING A FUNNEL</td>
+                            <td><strong>$50,000</strong> and 10% of gross sales of the funnel (lifetime of the funnel)</td>
+                          </tr>
+                          <tr>
+                            <td>TRAIN YOUR TEAM</td>
+                            <td><strong>$50,000</strong> for 2 days</td>
+                          </tr>
+                        </table>
+                        <span>** prices quoted are in USDVVV.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
