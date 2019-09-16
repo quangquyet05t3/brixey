52bbaa80e9b5fde44476c3b64993811e10c73472
quyet <quyet@whl.travel>
2019-09-17 06:46:36 +0700

    facebook

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -8,3 +8,6 @@
 <?php the_content() ?>
 
 
+<div id="fb-root"></div>
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v4.0&appId=443135326229554&autoLogAppEvents=1"></script>
+<div class="fb-comments" data-href="https://tranxuanvan.com/la-ma-quen-voi-ban-ve-thiet-ke-khong-gian-quan-cafe-yen-tai-ha-noi/" data-width="1000" data-numposts="5"></div>
